0f32578f930b643668a198a22601a9bac3692836
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2023-07-10 17:13:12 +0330

    Add parser package and define phrase structure
    Use composite pattern for phrases

---
+++ parser/phrase.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+
+class Phrase(ABC):
+    @abstractmethod
+    def get_value(self):
+        ...
+
+
+class Composite(Phrase):
+
+    def __init__(self, left_phrase, right_phrase, operator):
+        self.left_phrase: Optional[Phrase] = left_phrase
+        self.right_phrase: Optional[Phrase] = right_phrase
+        self.operator = operator
+
+    def get_value(self):
+            return self.operator(self.left_phrase.get_value(), self.right_phrase.get_value())
+
+
+class Leaf(Phrase):
+    def __init__(self):
+        self.value: float = 0
+
+    def get_value(self):
+        return self.value

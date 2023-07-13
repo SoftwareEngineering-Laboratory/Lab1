e8a464e7c6b21d99faeb6cf6108a704f5063a02f
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2023-07-13 12:56:02 +0330

    Fix view errors
    Fix parser name to parse in view package

--- view/terminal.py
+++ view/terminal.py
@@ -1,16 +1,16 @@
-from parser.arithmetic_exception import ArithmeticException
-from parser.parser import calculate_phrase
+from parse.arithmetic_exception import ArithmeticException
+from parse.parser import get_value_phrase
 
 
 def run_calculator():
-    print("enter your phrase! (enter <end> if you want to terminate program)")
     while True:
+        print("enter your phrase! (enter <end> if you want to terminate program)")
         phrase = input()
         if phrase == "end":
             print("shutting down the program!")
             break
         try:
-            output = calculate_phrase(phrase)
+            output = get_value_phrase(phrase)
         except ArithmeticException:
             print("your phrase has some arithmetic or semantic erroneous")
             continue
